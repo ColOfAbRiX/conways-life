28409fc4170d7c882f99b8300e669f4c5b8e6ee5
Fabrizio Colonna <colofabrix@tin.it>
2019-01-15 00:31:59 +0000

    Full implementation but doesn't calculate neighbours correctly.

--- scala/src/main/scala/com/colofabrix/scala/ConwaysLife.scala
+++ scala/src/main/scala/com/colofabrix/scala/ConwaysLife.scala
@@ -8,7 +8,7 @@ import scala.util.Random
 
 object ConwaysLife {
 
-  val gridSize = Coord( 10, 10 )
+  val gridSize = Coord( 6, 6 )
 
   /** Coordinates in the field */
   case class Coord( x: Int, y: Int )
@@ -16,59 +16,60 @@ object ConwaysLife {
   /** Type of the field of the game */
   type Grid = Store[Coord, Boolean]
 
-  /** Prints the field */
-  def render( plane: Grid ): String = {
-    val extent = 10
-
-    val coords: List[Coord] = (for {
-      x <- 0 until gridSize.x
-      y <- 0 until gridSize.y
-    } yield Coord(x, y)).toList
-
-    def cellString(value: Boolean): String =
-      if( value ) " X " else " . "
-
-    val cells = plane.experiment[List]( _ => coords ) map cellString
-
-    cells.grouped( extent )
-      .map(_.mkString)
-      .mkString("\n")
-  }
-
   /** Find the coordinates of the 8 neighbours on an infinite plane */
   def neighbours( c: Coord ): List[Coord] =
     for {
       dx <- List(-1, 0, 1)
-      dy <- List(-1, 0, 1)
-      if dx != dy
+      dy <- List(-1, 0, 1) if dx != dy
     } yield {
       Coord( c.x + dx, c.y + dy )
     }
 
   /** Function to determine the state of a cell */
   def conway( plane: Grid ): Boolean = {
-    var alive = plane
-      .experiment(neighbours)
-      .count(identity)
-    println( s"${plane.index}: $alive" )
-    false
+    val alive = plane
+      .experiment( neighbours )
+      .count( identity )
+
+    val next = plane.extract match {
+      case true if alive < 2 => false
+      case true if alive > 3 => false
+      case false if alive == 3 => true
+      case x => x
+    }
+
+    println(s"${plane.index}: ${plane.extract} + $alive -> $next")
+    next
+  }
+
+  /** Prints the field */
+  def render( plane: Grid ): String = {
+    val coords: List[Coord] = for {
+      x <- (0 until gridSize.x).toList
+      y <- (0 until gridSize.y).toList
+    } yield {
+      Coord(x, y)
+    }
+
+    plane.experiment( _ => coords )
+      .map( cell => if( cell ) " x" else " ." )
+      .grouped( gridSize.x )
+      .map( _.mkString )
+      .mkString( "\n" )
   }
 
   /** The main game loop */
-  @tailrec
   def gameLoop( current: Grid ): Grid  = {
-    //println( "\033\143" + render(current) )
-    Thread.sleep(1000)
-    println(" LOOP ")
+    println( "\033\143\n" + render(current) )
     val next = current.coflatMap( conway )
-    //gameLoop( next )
-    Store[Coord, Boolean](identity, Coord(0, 0))
+    Thread.sleep( 1000 )
+    gameLoop( next )
   }
 
   /** Start here */
   def main( args: Array[String] ): Unit = {
     val initial = { _: Coord =>
-      Random.nextInt(10) == 0
+      Random.nextInt(5) == 0
     }
     gameLoop( Store(initial, Coord(0, 0)) )
   }
